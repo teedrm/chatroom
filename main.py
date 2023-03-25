8b1a6b9b79fa7ea9e6b15ae9e3ccda53c77a170f
Thy Nguyen <thy.hn.nguyen@gmail.com>
2023-03-25 16:49:06 -0600

    set up room route

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, session, redirect
+from flask import Flask, render_template, request, session, redirect, url_for
 from flask_socketio import join_room, leave_room, send, SocketIO
 import random
 from string import ascii_uppercase
@@ -11,7 +11,7 @@ socketio = SocketIO(app)
 #store different rooms info
 rooms = {}
 
-def generate_unique_code(Length):
+def generate_unique_code(length):
     while True:
         code = ""
         for _ in range(length):
@@ -24,7 +24,9 @@ def generate_unique_code(Length):
 
 #homepage route
 @app.route("/", methods=["POST", "GET"])
-def home() :
+def home():
+    #prevent user to retype and navigate to another room from homepage
+    session.clear()
     if request.method == "POST":
         name = request.form.get("name")
         code = request.form.get("code")
@@ -32,10 +34,11 @@ def home() :
         create = request.form.get("create", False)
 
         if not name:
-            return render_template("home.html", error="Please enter a name")
+            #code=code, name=name because need to pass back to the code if page is refreshed - whatever user typed in before refresh
+            return render_template("home.html", error="Please enter a name", code=code, name=name)
         
         if join != False and not code:
-            return render_template("home.html", error="Please enter a room code")
+            return render_template("home.html", error="Please enter a room code", code=code, name=name)
         
         #check if room + exists
         room = code
@@ -43,10 +46,24 @@ def home() :
             room = generate_unique_code(4)
             #room and room data will be added to rooms dictionary
             rooms[room] = {"members": 0, "messages": []}
+        elif code not in rooms:
+            return render_template("home.html", error="Room does not exist", code=code, name=name)
+        
+        #use session to temporarily store data - can be manipulated by the server
+        session["room"] = room
+        session["name"] = name
+        return redirect(url_for("room"))
 
     return render_template("home.html")
 
 #roompage route
+def room():
+    #dont allow /room but has to join directly from join
+    room = session.get("room")
+    if room is None or session.get("name") is None or room not in rooms:
+        return redirect(url_for("home"))
+    
+    return render_template("room.html")
 
 if __name__ == "__main__":
     #debug true to auto refresh new changes
